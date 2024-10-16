9059a9573f6eb327f0c51282c4108988773c3fdc
Srikanth Pragada <srikanthpragada@gmail.com>
2024-10-16 09:13:11 +0530

    16-OCT

--- admission/test_lr_model.py
+++ admission/test_lr_model.py
@@ -6,5 +6,6 @@ gre = int(input("Enter GRE   : "))
 tof = int(input("Enter Toefl : "))
 cgpa = float(input("Enter CGPA : "))
 
-result = model.predict([[gre,tof,cgpa]])
+df = pd.DataFrame([[gre,tof,cgpa]], columns = ["Gre","Toefl","Cgpa"])
+result = model.predict(df) 
 print(f"{result[0]:.2f}")
